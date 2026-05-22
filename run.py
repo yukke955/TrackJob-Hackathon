79827f8a2d6aa9098249470476ae8ee736a1a16f
Yusuke Kago <kago.y.955e@m.isct.ac.jp>
2026-05-22 14:52:30 +0900

    fix vercel flask routing

--- run.py
+++ run.py
@@ -1,6 +1,14 @@
-from app import app, db
+from flask import Flask
 
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()  # DBファイルを作成
-    app.run(debug=True)
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello from Vercel!"
+
+# from app import app, db
+
+# if __name__ == "__main__":
+#     with app.app_context():
+#         db.create_all()  # DBファイルを作成
+#     app.run(debug=True)
